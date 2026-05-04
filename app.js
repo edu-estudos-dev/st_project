c0174ae78e4f46a9647d11c9845a5e5b9067f24e
edu.estudos.dev <estudos.edu.dev@gmail.com>
2026-05-04 14:51:59 -0300

    melhorando desempenho 2

--- app.js
+++ app.js
@@ -74,7 +74,9 @@ app.use(sitemapRoutes);
 
 app.use(
   express.static(path.join(__dirname, 'public'), {
-    etag: false
+    etag: true,
+    maxAge: '30d',
+    immutable: true
   })
 );
 
@@ -87,12 +89,15 @@ const legacyImageRedirects = {
   '/favicon-96x96.png': '/images/system/favicon-96x96.png',
   '/images/antonio-ferreira.webp': '/images/home/antonio-ferreira.webp',
   '/images/bolinhas.webp': '/images/home/bolinhas.webp',
-  '/images/capa-rota-maquinas-recreativas.webp': '/images/blog/capa-rota-maquinas-recreativas.webp',
+  '/images/capa-rota-maquinas-recreativas.webp':
+    '/images/blog/capa-rota-maquinas-recreativas.webp',
   '/images/consignados.webp': '/images/home/consignados.webp',
-  '/images/dashboard-vendmaster-mobile.webp': '/images/home/dashboard-vendmaster-mobile.webp',
+  '/images/dashboard-vendmaster-mobile.webp':
+    '/images/home/dashboard-vendmaster-mobile.webp',
   '/images/dashboard-vendmaster.webp': '/images/home/dashboard-vendmaster.webp',
   '/images/eduardo-praciano.webp': '/images/home/eduardo-praciano.webp',
-  '/images/garantia-30-dias-vendmaster.webp': '/images/home/garantia-30-dias-vendmaster.webp',
+  '/images/garantia-30-dias-vendmaster.webp':
+    '/images/home/garantia-30-dias-vendmaster.webp',
   '/images/gfgfgfg.webp': '/images/system/gfgfgfg.webp',
   '/images/ggfgfgfg.webp': '/images/system/ggfgfgfg.webp',
   '/images/joao-ribeiro.webp': '/images/home/joao-ribeiro.webp',
@@ -102,9 +107,11 @@ const legacyImageRedirects = {
   '/images/og-image.png': '/images/brand/og-image.png',
   '/images/painel-vendmaster.webp': '/images/home/painel-vendmaster.webp',
   '/images/pelucias.webp': '/images/home/pelucias.webp',
-  '/images/rota-vendmaster-mobile.webp': '/images/home/rota-vendmaster-mobile.webp',
+  '/images/rota-vendmaster-mobile.webp':
+    '/images/home/rota-vendmaster-mobile.webp',
   '/images/rota-vendmaster.webp': '/images/home/rota-vendmaster.webp',
-  '/web-app-manifest-192x192.png': '/images/system/web-app-manifest-192x192.png',
+  '/web-app-manifest-192x192.png':
+    '/images/system/web-app-manifest-192x192.png',
   '/web-app-manifest-512x512.png': '/images/system/web-app-manifest-512x512.png'
 };
 
@@ -158,7 +165,11 @@ app.use(
 
 app.use(
   '/vendor/bootstrap',
-  express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist'))
+  express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist'), {
+    etag: true,
+    maxAge: '30d',
+    immutable: true
+  })
 );
 
 app.use(
